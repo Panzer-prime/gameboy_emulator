f8f06aeada9ffade97fcf438eabd9840c46aa583
Panzer <119689044+Panzer-prime@users.noreply.github.com>
2024-05-27 17:27:29 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/projects/gameboy_emulator
-# Build directory: F:/projects/gameboy_emulator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
